a451890ba09ff7954584bcd0698386cbf091fa26
poylar <doaleks8@gmail.com>
2021-12-09 23:17:25 +0800

    qwe

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 import '../scss/app.scss';
 
-function requireAll(r) {
-  // get all svg
-  r.keys().forEach(r);
-}
-
-requireAll(require.context('../images/svg/', true, /\.svg$/));
+const delegateHandler = (selector, handler) => (event) => {
+  const possibleTarget = event.target.closest(selector);
+  if (possibleTarget) {
+    event.delegateTarget = possibleTarget;
+    handler(event);
+  }
+};
 
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
@@ -22,5 +23,40 @@ const roundBlock = document.querySelector('.round-svg');
 window.addEventListener('scroll', () => {
   if (isInViewport(roundBlock)) {
     roundBlock.classList.add('svg-animated');
+  }
+});
+
+document.addEventListener(
+  'click',
+  delegateHandler('.faq__item', (event) => {
+    let innerHeight = event.delegateTarget
+      .querySelector('.faq__content')
+      .getBoundingClientRect().height;
+    console.log(innerHeight);
+    let item = event.delegateTarget.querySelector('.faq__content-wrapper');
+    if (event.delegateTarget.classList.contains('active')) {
+      item.style.height = `0`;
+      event.delegateTarget.classList.remove('active');
+    } else {
+      item.style.height = `${innerHeight}px`;
+      event.delegateTarget.classList.add('active');
+    }
+  })
+);
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.grid__item[data-vacancy]')) {
+    const vacancy = e.target.dataset.vacancy;
+    console.log(vacancy);
+    const modal = document.querySelector(
+      `.modal__vacancy[data-vacancy="${vacancy}"`
+    );
+    document.querySelector('.modal__wrapper').classList.add('active');
+    modal.classList.add('active');
+  } else if (e.target.closest('.modal__wrapper .close') || !e.target.closest('.modal__vacancy')) {
+    document.querySelector('.modal__wrapper').classList.remove('active');
+    document.querySelectorAll('.modal__vacancy').forEach((elem) => {
+      elem.classList.remove('active');
+    });
   }
 });
