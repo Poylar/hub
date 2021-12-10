352cfb16574257b146aaabca3e424c69f889cba1
poylar <doaleks8@gmail.com>
2021-12-10 17:28:26 +0800

    qwe

--- src/js/app.js
+++ src/js/app.js
@@ -10,13 +10,7 @@ const delegateHandler = (selector, handler) => (event) => {
 
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
 
 const roundBlock = document.querySelector('.round-svg');
@@ -29,9 +23,7 @@ window.addEventListener('scroll', () => {
 document.addEventListener(
   'click',
   delegateHandler('.faq__item', (event) => {
-    let innerHeight = event.delegateTarget
-      .querySelector('.faq__content')
-      .getBoundingClientRect().height;
+    let innerHeight = event.delegateTarget.querySelector('.faq__content').getBoundingClientRect().height;
     console.log(innerHeight);
     let item = event.delegateTarget.querySelector('.faq__content-wrapper');
     if (event.delegateTarget.classList.contains('active')) {
@@ -44,19 +36,21 @@ document.addEventListener(
   })
 );
 
+const modalWrapper = document.querySelector('.modal__wrapper');
+const modal = document.querySelectorAll('.modal__item');
 document.addEventListener('click', (e) => {
-  if (e.target.closest('.grid__item[data-vacancy]')) {
-    const vacancy = e.target.dataset.vacancy;
-    console.log(vacancy);
-    const modal = document.querySelector(
-      `.modal__vacancy[data-vacancy="${vacancy}"`
-    );
-    document.querySelector('.modal__wrapper').classList.add('active');
-    modal.classList.add('active');
-  } else if (e.target.closest('.modal__wrapper .close') || !e.target.closest('.modal__vacancy')) {
-    document.querySelector('.modal__wrapper').classList.remove('active');
-    document.querySelectorAll('.modal__vacancy').forEach((elem) => {
+  const target = e.target.closest('.grid__item[data-vacancy]');
+  if ((modalWrapper.classList.contains('active') && !e.target.closest('.modal__item')) || e.target.closest('.close')) {
+    [modalWrapper, modal].forEach((elem) => {
       elem.classList.remove('active');
     });
+  } else if (target) {
+    const vacancy = target.dataset.vacancy;
+    const modalType = document.querySelector(`.modal__vacancy[data-vacancy="${vacancy}"`);
+    if (modalType) {
+      [modalWrapper, modalType].forEach((elem) => {
+        elem.classList.add('active');
+      });
+    }
   }
 });
